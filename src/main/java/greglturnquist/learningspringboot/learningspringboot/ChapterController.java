257681479091af944802386bdb5263be81ce946c
Michael Lennon <kali.mist@gmail.com>
2018-11-10 13:44:18 +0000

    initial commit

---
+++ src/main/java/greglturnquist/learningspringboot/learningspringboot/ChapterController.java
@@ -0,0 +1,18 @@
+package greglturnquist.learningspringboot.learningspringboot;
+
+import lombok.var;
+import reactor.core.publisher.Flux;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class ChapterController {
+    private final ChapterRepository repository;
+    public ChapterController(ChapterRepository repository) {
+        this.repository = repository;
+    }
+    @GetMapping("/chapters")
+    public Flux<Chapter> listing() {
+        return repository.findAll();
+
+    }
+}
